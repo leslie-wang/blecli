47af9a7eb57eed5623f69ddbd665994262bab89e
Leslie Wang <wqyuwss@gmail.com>
2025-05-01 22:25:57 -0700

    add new protocol over ble
    Signed-off-by: Leslie Wang <wqyuwss@gmail.com>

--- ble_server.py
+++ ble_server.py
@@ -1,61 +1,132 @@
 import uasyncio as asyncio
 import aioble
 import bluetooth
+import os
+import struct
 
 aioble.log_level=2
 ble_apprearance = 0x0300
 ble_advertiseing_interval = 2000
 
 # BLE UUIDs
-SERVICE_UUID = bluetooth.UUID(0x181A)     # Environmental Sensing
+SERVICE_UUID = bluetooth.UUID(0x1234)     # Environmental Sensing
 WRITE_CHAR_UUID = bluetooth.UUID(0x6e40)   # Temperature (write)
 NOTIFY_CHAR_UUID = bluetooth.UUID(0x6e41)  # Notify
 
-# Create BLE service and characteristics
-'''
-service = aioble.Service(SERVICE_UUID)
+FILE_DIR = "/"
 
-write_char = aioble.Characteristic(
-    service,
-    WRITE_CHAR_UUID,
-    read=True,
-    write=True,
-    write_no_response=True,
-)
+async def handle_echo(notify_char, conn, data):
+    print("received echo request, echo back: ", data)
+    notify_char.notify(conn, data)  # Echo back data
+    await asyncio.sleep(3)
+    
+# === File listing formatter ===
+def handle_list_files(notify_char, conn):
+    entries = []
+    for name in os.listdir(FILE_DIR):
+        try:
+            print("stat:", name)
+            size = os.stat(FILE_DIR + "/" + name)[6]
+            entries.append(f"{name},{size}")
+        except:
+            continue
+    response = ";".join(entries)
+    notify_char.notify(conn, response.encode())
+    await asyncio.sleep(3)
+    
+async def handle_get_file(notify_char, conn, data):
+    print("get file: ", data)
+    try:
+        with open(data, "rb") as f:
+            buf = f.read()
+            notify_char.notify(conn, buf)  # Echo file content
+            await asyncio.sleep(3)
+    except OSError as e:
+        print("Failed to read file:", e)
+      
+async def handle_save_file(notify_char, conn, data):
+    if len(data) < 21:
+        print(f"[UPLOAD] err: too short")
+        notify_char.notify(conn, b"ERR:Too short")
+        return
 
-notify_char = aioble.Characteristic(
-    service,
-    NOTIFY_CHAR_UUID,
-    read=True,
-    notify=True,
-)
+    md5name = binascii.hexlify(data[:16]).decode()
+    
+    file_size = struct.unpack(">I", data[16:20])[0]
+    content = data[20:]
+    content_size = len(content)
+    if content_size != file_size:
+        print(f"[UPLOAD] Filename={md5name}: wrong size {file_size} != {content_size}")
+        notify_char.notify(conn, b"ERR:Size mismatch")
+        return
+    
+    print(f"[UPLOAD] Filename={md5name} Size={file_size}")
 
-# Register service
-aioble.register_services(service)
-'''
-'''
-def run_service():
-    # Create BLE service and characteristics
-    service = aioble.Service(SERVICE_UUID)
+    try:
+        with open(FILE_DIR + "/" + md5name, "wb") as f:
+            f.write(content)
+        notify_char.notify(conn, b"ACK:OK")
+    except Exception as e:
+        notify_char.notify(conn, "ERR:{str(e)}".encode())
+            
+async def handle_delete_file(notify_char, conn, data):
+    if len(data) != 20:
+        print(f"[DELETE] wrong length {data}")
+        notify_char.notify(conn, b"ERR:WRONG REQUEST")
+        return
+    
+    try:
+        md5hash = binascii.hexlify(data[:16]).decode()
+        req = struct.unpack(">I", data[16:])[0]
+    
+        filepath = FILE_DIR + "/" + filename
+        
+        if not os.path.exists(filepath):
+            print(f"[DELETE] {filepath}: not found")
+            notify_char.notify(conn, b"ERR:NOT_FOUND")
+            return
+        
+        size = os.stat(filename)[6]
+        
+        if req != size:
+            print(f"[DELETE] {filepath}: wrong size {req} != {size}")
+            notify_char.notify(conn, b"ERR:WRONG REQUEST")
+            return
+    
+        os.remove(filepath)
+        notify_char.notify(conn, b"ACK:DELETED")
+            
+    except Exception as e:
+        print("delete file: ", e)
+            
+async def handle_data(notify_char, conn, data):
+    method = data[0]
+    print("received method: ", method)
 
-    global write_char = aioble.Characteristic(
-        service,
-        WRITE_CHAR_UUID,
-        read=True,
-        write=True,
-        write_no_response=True,
-    )
+    # Method 0: Echo (health check)
+    if method == 0:
+        await handle_echo(notify_char, conn, data[1:])
 
-    global notify_char = aioble.Characteristic(
-        service,
-        NOTIFY_CHAR_UUID,
-        read=True,
-        notify=True,
-    )
+    # Method 1: File upload
+    elif method == 1:
+        await handle_save_file(notify_char, conn, data[1:])
 
-    # Register service
-    aioble.register_services(service)
-'''
+    # Method 2: File delete
+    elif method == 2:
+        await handle_delete_file(notify_char, conn, data[1:])
+
+    # Method 3: File list
+    elif method == 3:
+        handle_list_files(notify_char, conn)
+        
+    # Method 4: File list
+    elif method == 4:
+        await handle_get_file(notify_char, conn, data[1:])
+
+    else:
+        await send_notify(b"ERR:Unknown method")
+
+
 async def writer_loop(write_char, notify_char, conn):
     try:
         while conn.is_connected():
@@ -64,9 +135,9 @@ async def writer_loop(write_char, notify_char, conn):
             data = write_char.read()
             print("Got write:", data)
             if conn.is_connected():
-                notify_char.notify(conn, b"ACK: hi")
-                print("Notify sent successfully")
-                await asyncio.sleep(3)
+                await handle_data(notify_char, conn, data)
+            else:
+                print("Connection lost")
     except asyncio.CancelledError:
         print("Writer task cancelled")
     except Exception as e:
@@ -104,12 +175,11 @@ async def connection_handler():
                 ble_advertiseing_interval,
                 name="pico2w_ble",
                 services=[SERVICE_UUID],
-                appearance=ble_apprearance,
-                timeout_ms=30000
+                appearance=ble_apprearance
             ) as conn:
                 print("Connected to:", conn.device)
                 
-                writer_task = asyncio.create_task(writer_loop(write_char, notify_char, conn))
+                writer_task = asyncio.create_task(writer_loop(write_char, notify_char,conn))
 
                 while conn.is_connected():
                     await asyncio.sleep(0.5)
--- main.go
+++ main.go
@@ -1,170 +1,304 @@
 package main
 
 import (
+	"crypto/md5"
+	"errors"
 	"fmt"
-	"log"
 	"os"
+	"strconv"
 	"time"
 
-	"github.com/pkg/errors"
 	"github.com/urfave/cli"
 	"tinygo.org/x/bluetooth"
 )
 
 var adapter = bluetooth.DefaultAdapter
-
-func main() {
-	app := cli.NewApp()
-	app.Usage = "BLE client tools"
-
-	app.Commands = []cli.Command{
-		{
-			Name:    "scan",
-			Aliases: []string{"d"},
-			Action:  scan,
-		},
-		{
-			Name:    "list-photos",
-			Aliases: []string{"lp"},
-			Action:  listPhotos,
-		},
-	}
-
-	if err := app.Run(os.Args); err != nil {
-		log.Fatal(err)
-	}
-}
-
-func listPhotos(ctx *cli.Context) error {
-	return nil
-}
-
-func scan(ctx *cli.Context) error {
-	// Enable BLE interface.
-	err := adapter.Enable()
-	if err != nil {
-		return err
-	}
-
-	// Start scanning.
-	var deviceAddress bluetooth.Address
-	found := false
-	println("scanning...")
-	err = adapter.Scan(func(adapter *bluetooth.Adapter, device bluetooth.ScanResult) {
-		if device.LocalName() != "pico2w_ble" {
-			return
-		}
-		fmt.Printf("found device: %s, RSSI: %d, %s\n", device.Address.String(), device.RSSI, device.LocalName())
-		deviceAddress = device.Address
-		found = true
-		adapter.StopScan()
-	})
-	if err != nil {
-		return err
-	}
-
-	if !found {
-		return errors.New("PicoServer not found")
-	}
-
-	fmt.Println("Connecting to PicoServer...")
-	device, err := adapter.Connect(deviceAddress, bluetooth.ConnectionParams{})
-	if err != nil {
-		log.Fatalf("Failed to connect: %v", err)
-	}
-	defer device.Disconnect()
-
-	// Discover services and characteristics
-	// # On the Pico W (MicroPython side)
-	// ble_svc_uuid = bluetooth.UUID(0x181A)         # Environmental Sensing Service
-	// ble_characteristic_uuid = bluetooth.UUID(0x2A6E)  # Temperature Characteristic
-
-	serviceUUID := baseUUID(0x181A)
-	writeUUID := baseUUID(0x6E40).String()
-	notifyUUID := baseUUID(0x6E41).String()
-
-	fmt.Println("Discovering Services PicoServer...")
-	services, err := device.DiscoverServices([]bluetooth.UUID{serviceUUID})
-	if err != nil {
-		return err
-	}
-	if len(services) == 0 {
-		return errors.New("service not found")
-	}
-
-	fmt.Printf("Discovered Services: %+v\n", services)
-
-	// Discover characteristics
-	fmt.Println("Discovering characteristics PicoServer...")
-
-	chars, err := services[0].DiscoverCharacteristics(nil)
-	if err != nil {
-		return errors.Wrapf(err, "Could not find notify characteristic")
-	}
-
-	var notifyChar, writeChar *bluetooth.DeviceCharacteristic
-	for _, char := range chars {
-		switch char.UUID().String() {
-		case writeUUID:
-			writeChar = &char
-		case notifyUUID:
-			notifyChar = &char
-		}
-	}
-	if notifyChar == nil {
-		return errors.New("unable to discover notification characteristics")
-	} else if writeChar == nil {
-		return errors.New("unable to discover write characteristics")
-	}
-
-	fmt.Printf("Discovered Notify Characteristics: %+v\n", notifyChar)
-
-	done := make(chan struct{})
-	// Enable notifications before sending the request
-	notifyChar.EnableNotifications(func(buf []byte) {
-		fmt.Println("Received response:", string(buf))
-		done <- struct{}{}
-	})
-
-	time.Sleep(1 * time.Second)
-
-	fmt.Printf("Discovered Write Characteristics: %+v\n", writeChar)
-
-	// Send "Hello, world"
-	fmt.Println("send hello world")
-	for _, message := range []string{"hello", "world"} {
-		_, err = writeChar.WriteWithoutResponse([]byte(message))
-		if err != nil {
-			log.Fatalf("Failed to write: %v", err)
-		}
-		select {
-		case <-done:
-		case <-time.After(time.Second):
-			// resend
-			fmt.Printf("Message (%s) sent again.\n", message)
-			_, err = writeChar.WriteWithoutResponse([]byte(message))
-			if err != nil {
-				log.Fatalf("Failed to write: %v", err)
-			}
-			time.Sleep(3 * time.Second)
-
-			fmt.Printf("Message (%s) sent 3rd time.\n", message)
-			_, err = writeChar.WriteWithoutResponse([]byte(message))
-			if err != nil {
-				log.Fatalf("Failed to write: %v", err)
-			}
-		}
-		time.Sleep(3 * time.Second)
-		fmt.Printf("Message (%s) sent successfully.\n", message)
-
-	}
-	time.Sleep(10 * time.Second)
-	return nil
-}
+var serviceUUID = baseUUID(0x1234)
+var writeUUID = baseUUID(0x6e40)
+var notifyUUID = baseUUID(0x6e41)
+var advName = "pico2w_ble"
 
 func baseUUID(short uint16) bluetooth.UUID {
 	var b = [16]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x10, 0x00, 0x80, 0x00, 0x00, 0x80, 0x5F, 0x9B, 0x34, 0xFB}
 	b[2] = byte(short >> 8)
 	b[3] = byte(short & 0xFF)
 	return bluetooth.NewUUID(b)
+}
+
+type methodType byte
+
+const (
+	echo methodType = iota
+	uploadImage
+	deleteImage
+	listImages
+	getImage
+)
+
+func (m methodType) String() string {
+	switch m {
+	case echo:
+		return "echo"
+	case uploadImage:
+		return "upload"
+	case deleteImage:
+		return "delete"
+	case listImages:
+		return "list"
+	case getImage:
+		return "get"
+	default:
+		return "unknown"
+	}
+}
+
+func main() {
+	app := &cli.App{
+		Name:  "bleclient",
+		Usage: "BLE Client for file operations",
+		Commands: []cli.Command{
+			{
+				Name:   "scan",
+				Usage:  "Scan all qualified devices",
+				Action: scan,
+			},
+			{
+				Name:   "echo",
+				Usage:  "Send echo message",
+				Action: runEcho,
+			},
+			{
+				Name:   "upload",
+				Usage:  "Upload a file",
+				Action: runUpload,
+			},
+			{
+				Name:   "delete",
+				Usage:  "Delete a file by MD5",
+				Action: runDelete,
+			},
+			{
+				Name:   "list",
+				Usage:  "List all files",
+				Action: runList,
+			},
+			{
+				Name:   "get",
+				Usage:  "Get one file",
+				Action: runGetFile,
+			},
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		fmt.Fprintln(os.Stderr, "Error:", err)
+	}
+}
+func scan(c *cli.Context) error {
+	err := adapter.Enable()
+	if err != nil {
+		return err
+	}
+
+	return adapter.Scan(func(adapter *bluetooth.Adapter, result bluetooth.ScanResult) {
+		if result.LocalName() != advName {
+			return
+		}
+		fmt.Printf("found device: %s, RSSI: %d, %s\n", result.Address.String(), result.RSSI, result.LocalName())
+	})
+}
+
+func connectToPeripheral() (*bluetooth.Device, *bluetooth.DeviceCharacteristic,
+	*bluetooth.DeviceCharacteristic, error) {
+	err := adapter.Enable()
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	var found bool
+	var deviceAddress bluetooth.Address
+	err = adapter.Scan(func(adapter *bluetooth.Adapter, result bluetooth.ScanResult) {
+		if result.LocalName() != advName {
+			return
+		}
+		fmt.Printf("found device: %s, RSSI: %d, %s\n", result.Address.String(), result.RSSI, result.LocalName())
+		deviceAddress = result.Address
+		found = true
+		adapter.StopScan()
+	})
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	if !found {
+		return nil, nil, nil, errors.New("PicoServer not found")
+	}
+
+	device, err := adapter.Connect(deviceAddress, bluetooth.ConnectionParams{})
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	services, err := device.DiscoverServices([]bluetooth.UUID{serviceUUID})
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	writeChars, err := services[0].DiscoverCharacteristics([]bluetooth.UUID{writeUUID})
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	notiChars, err := services[0].DiscoverCharacteristics([]bluetooth.UUID{notifyUUID})
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	return &device, &writeChars[0], &notiChars[0], nil
+}
+
+func writeWithDelay(char *bluetooth.DeviceCharacteristic, data []byte) error {
+	_, err := char.WriteWithoutResponse(data)
+	time.Sleep(3 * time.Second)
+	return err
+}
+
+func writeHeader(char *bluetooth.DeviceCharacteristic, m methodType) error {
+	return writeWithDelay(char, []byte{byte(m)})
+}
+
+func sendRequest(char *bluetooth.DeviceCharacteristic, m methodType, body []byte) error {
+	fmt.Printf("Sending method %s and content (%dB)\n", m, len(body))
+
+	return writeWithDelay(char, append([]byte{byte(m)}, body...))
+}
+
+func runEcho(c *cli.Context) error {
+	device, writeChar, notifyChar, err := connectToPeripheral()
+	if err != nil {
+		return err
+	}
+	defer device.Disconnect()
+
+	// Enable notifications before sending the request
+	notifyChar.EnableNotifications(func(buf []byte) {
+		fmt.Println("Received response:", string(buf))
+	})
+
+	time.Sleep(time.Second)
+
+	err = sendRequest(writeChar, echo, []byte("ping!"))
+	if err != nil {
+		return err
+	}
+
+	return sendRequest(writeChar, echo, []byte("pong!"))
+}
+
+func prepareFileHeader(md5hex string, v uint32) ([]byte, error) {
+	buf := make([]byte, 16)
+	_, err := fmt.Sscanf(md5hex, "%x", &buf)
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Printf("md5sum: %s\n", md5hex)
+
+	buf = append(buf, byte(v>>24))
+	buf = append(buf, byte(v>>16))
+	buf = append(buf, byte(v>>8))
+	buf = append(buf, byte(v))
+
+	return buf, nil
+}
+
+func runUpload(c *cli.Context) error {
+	filename := c.Args()[0]
+	content, err := os.ReadFile(filename)
+	if err != nil {
+		return err
+	}
+
+	buf, err := prepareFileHeader(fmt.Sprintf("%x", md5.Sum(content)), uint32(len(content)))
+	if err != nil {
+		return err
+	}
+
+	device, writeChar, notifyChar, err := connectToPeripheral()
+	if err != nil {
+		return err
+	}
+	defer device.Disconnect()
+
+	notifyChar.EnableNotifications(func(buf []byte) {
+		fmt.Println("Received upload response:", string(buf))
+	})
+
+	return sendRequest(writeChar, uploadImage, append(buf, content...))
+}
+
+func runDelete(c *cli.Context) error {
+	md5hex := c.Args()[0]
+	if len(md5hex) != 32 {
+		return fmt.Errorf("MD5 must be 32 hex characters")
+	}
+
+	size, err := strconv.Atoi(c.Args()[1])
+	if err != nil {
+		return err
+	}
+
+	payload, err := prepareFileHeader(md5hex, uint32(size))
+	if err != nil {
+		return err
+	}
+
+	device, writeChar, notifyChar, err := connectToPeripheral()
+	if err != nil {
+		return err
+	}
+	defer device.Disconnect()
+
+	notifyChar.EnableNotifications(func(buf []byte) {
+		fmt.Println("Received delete image response:", string(buf))
+	})
+
+	return sendRequest(writeChar, deleteImage, payload)
+}
+
+func runList(c *cli.Context) error {
+	device, writeChar, notifyChar, err := connectToPeripheral()
+	if err != nil {
+		return err
+	}
+	defer device.Disconnect()
+
+	notifyChar.EnableNotifications(func(buf []byte) {
+		fmt.Println("Received list image response:", string(buf))
+	})
+
+	err = writeHeader(writeChar, listImages)
+	if err != nil {
+		return err
+	}
+	time.Sleep(time.Second)
+	return nil
+}
+
+func runGetFile(c *cli.Context) error {
+	device, writeChar, notifyChar, err := connectToPeripheral()
+	if err != nil {
+		return err
+	}
+	defer device.Disconnect()
+
+	notifyChar.EnableNotifications(func(buf []byte) {
+		fmt.Println("Received get image response:", string(buf))
+	})
+
+	err = sendRequest(writeChar, getImage, []byte(c.Args()[0]))
+	if err != nil {
+		return err
+	}
+	time.Sleep(time.Second)
+	return nil
 }
